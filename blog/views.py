bd273d0e2888ffcd1908068f8ef8d3273f8f74fd
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-06-11 23:27:08 -0300

    Update

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 
-def home (request):
-    return render (request, 'blog/home.html', {})
+def post_list(request):
+    return render (request, 'blog/post_list.html', {})
