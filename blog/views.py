b750a965b418dbab47298a70b142838dbd82047b
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-06-15 23:18:08 -0300

    Update URLs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 
 def post_list(request):
     posts = Post.objects.all()
-    return render (request, 'blog/post_list.html', {'posts' : posts})
+    users = User.objects.all()
+    return render (request, 'blog/post_list.html', {'posts' : posts, 'users': users})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def author_perfil(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=author)
+
+    return render(request, 'blog/author_perfil.html', {'author':author, 'posts': posts})
