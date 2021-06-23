eea77668e731a9813639e6134303aa769ba49eca
Adrian-Batista <62628581+Adrian-Batista@users.noreply.github.com>
2021-06-22 22:55:59 -0300

    Up

--- blog/urls.py
+++ blog/urls.py
@@ -1,20 +1,9 @@
 from django.urls import path
-from . import views
+from .import views
 
 urlpatterns = [
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('author/<username>/', views.author_perfil, name='author_perfil'),
-<<<<<<< HEAD
     path('drafts/', views.post_draft_list, name='post_draft_list'),
-=======
-    path('', views.post_list, name='post_list')
->>>>>>> parent of b750a96 (Update URLs)
-=======
-    path('', views.post_list, name='post_list')
->>>>>>> parent of b750a96 (Update URLs)
-=======
->>>>>>> parent of 390d1b9 (Rascunhos Blog)
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 def post_list(request):
     posts = Post.objects.all()
-<<<<<<< HEAD
-<<<<<<< HEAD
     users = User.objects.all()
     return render (request, 'blog/post_list.html', {'posts' : posts, 'users': users})
 
@@ -15,18 +16,8 @@ def post_detail(request, pk):
 def author_perfil(request, username):
     author = get_object_or_404(User, username=username)
     posts = Post.objects.filter(author=author)
-
     return render(request, 'blog/author_perfil.html', {'author':author, 'posts': posts})
-<<<<<<< HEAD
 
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
-=======
-    return render (request, 'blog/post_list.html', {'posts' : posts})
->>>>>>> parent of b750a96 (Update URLs)
-=======
-    return render (request, 'blog/post_list.html', {'posts' : posts})
->>>>>>> parent of b750a96 (Update URLs)
-=======
->>>>>>> parent of 390d1b9 (Rascunhos Blog)
